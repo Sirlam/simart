0f44f25cd9ea7fd5a73988db6b86c82cdce9e5cb
Siyanbola Akinola <akinola.siyanbola@gmail.com>
2017-06-29 16:59:10 +0100

    Added Pump and tanks connections

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,4 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import BankAccount, CrudeType, OnAccount, Station
+
+admin.site.register(BankAccount)
+admin.site.register(CrudeType)
+admin.site.register(OnAccount)
+admin.site.register(Station)
 
--- catalog/models.py
+++ catalog/models.py
@@ -83,3 +83,92 @@ class BankAccount(models.Model):
         """
         return '%s (%s)' % (self.name, self.account_number)
 
+
+#  Models for tanks and sales
+
+
+class Tank(models.Model):
+    """
+    Model representing Tanks belonging to each filling stations
+    """
+
+    tank_id = models.UUIDField(primary_key=True, default=uuid.uuid4, help_text="Unique ID for Tanks")
+    tank = models.CharField(max_length=200)
+    crude_type = models.ForeignKey(CrudeType, on_delete=models.SET_NULL, null=True)
+
+    def __str__(self):
+        """
+        String for representing the Model object (in Admin site etc.)
+        """
+        return '%s (%s)' % (self.tank, self.crude_type)
+
+
+class StationTank(models.Model):
+    """
+    Model representing or connecting tanks to a particular station
+    """
+    tank = models.ForeignKey(Tank, on_delete=models.SET_NULL, null=True)
+    station = models.ForeignKey(Station, on_delete=models.SET_NULL, null=True)
+
+    class Meta:
+        unique_together = ("station", "tank")
+
+    def __str__(self):
+        """
+        String for representing the Model object (in Admin site etc.)
+        """
+        return '%s (%s)' % (self.tank, self.station)
+
+
+class TankQuantity(models.Model):
+    """
+    Model for Tank Quantity
+    """
+    tank = models.ForeignKey(StationTank, on_delete=models.SET_NULL, null=True)
+    opening = models.PositiveIntegerField()
+    closing = models.PositiveIntegerField()
+    under = models.PositiveIntegerField()
+    pump = models.DecimalField(max_digits=6, decimal_places=2)
+    new = models.PositiveIntegerField(blank=True, null=True)
+    date_added = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        """
+        String for representing the Model object (in Admin site etc.)
+        """
+        return self.tank
+
+
+class Pump(models.Model):
+    """
+    Model representing Fuel Pumps
+    """
+    pump_id = models.UUIDField(primary_key=True, default=uuid.uuid4, help_text="Unique ID for Pumps")
+    crude_type = models.ForeignKey(CrudeType, on_delete=models.SET_NULL, null=True)
+    name = models.CharField(max_length=200)
+
+    class Meta:
+        unique_together = ("crude_type", "name")
+
+    def __str__(self):
+        """
+        String for representing the Model object (in Admin site etc.)
+        """
+        return '%s (%s)' % (self.name, self.crude_type)
+
+
+class StationPump(models.Model):
+    """
+    Models associating Pump to station and Tanks
+    """
+    tank = models.ForeignKey(StationTank, on_delete=models.SET_NULL, null=True)
+    pump = models.ForeignKey(Pump, on_delete=models.SET_NULL, null=True)
+
+    class Meta:
+        unique_together = ("tank", "pump")
+
+    def __str__(self):
+        """
+        String for representing the Model object (in Admin site etc.)
+        """
+        return '%s (%s)' % (self.pump, self.tank)
