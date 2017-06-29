3d984868686ecccfe3f755cd6365eb93cefa7a9c
Siyanbola Akinola <akinola.siyanbola@gmail.com>
2017-06-29 16:25:34 +0100

    Added dj-staic, updated settings.py

--- simart/settings.py
+++ simart/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'simart.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'simartDB',
+        'USER': 'postgres',
+        'PASSWORD': 'foshanFfer5',
+        'HOST': '/tmp',
+        'PORT': '5433',
     }
 }
 
@@ -118,4 +122,5 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
+STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
