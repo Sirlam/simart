3dcb186c4af857d8daca7e0ceec4cafd15077192
Siyanbola Akinola <akinola.siyanbola@gmail.com>
2017-06-29 17:39:33 +0100

    Added admin for all models, custom admin for Pumpsale

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,10 +1,26 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import BankAccount, CrudeType, OnAccount, Station
+from .models import BankAccount, CrudeType, OnAccount, Pump, Station, StationPump, StationTank, Tank, TankQuantity, DepositAmount, OnAccountSale, PumpSale, Voucher
 
 admin.site.register(BankAccount)
 admin.site.register(CrudeType)
 admin.site.register(OnAccount)
 admin.site.register(Station)
+admin.site.register(Pump)
+admin.site.register(StationTank)
+admin.site.register(StationPump)
+admin.site.register(Tank)
+admin.site.register(TankQuantity)
+admin.site.register(DepositAmount)
+admin.site.register(OnAccountSale)
+#  admin.site.register(PumpSale)
+admin.site.register(Voucher)
+
+@admin.register(PumpSale)
+class PumpSaleAdmin(admin.ModelAdmin):
+    list_display = ('station', 'pump', 'opening', 'closing', 'rate', 'sale_add_date')
+
+
+
 
