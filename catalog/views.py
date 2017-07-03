fa1944251059bc75289a08a9158d28a2b3db5c48
Sirlam <salam_lanre@yahoo.com>
2017-07-03 22:30:24 +0100

    Did base templating for the home page

--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from .models import Station, TankQuantity, PumpSale, OnAccountSale
+
+
+def index(request):
+    """
+    View function for the home page site
+    """
+    # TODO: Generate station name
+
+    # Render the HTML template index.html with the data in the context variable
+    return render(
+        request,
+        'index.html',
+        context={},
+    )
+
